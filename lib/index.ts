9ed8dcdbc7dacc2b9bedf65676628d428ae05cac
Nobuyuki Kondo <kondo@iki-inc.net>
2022-02-27 10:54:32 +0900

    feat: Adding logic

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,69 @@
-console.log('init')
+export default class Fuzzy {
+  /**
+   * Line of Fuzzy Methods
+   * @param value - target
+   * @param min - stating point
+   * @param max - last stop
+   * @param reverse - if true, reverse the result
+   */
+  static grade(
+    value: number,
+    min: number,
+    max: number,
+    reverse = false
+  ): number {
+    if (value <= min) return !reverse ? 0 : 1
+    if (value >= max) return !reverse ? 1 : 0
+
+    const diff = max - min
+
+    if (reverse) {
+      // is reverse
+      return max / diff - value / diff
+    }
+
+    return value / diff - min / diff
+  }
+
+  /**
+   * Triangle of Fuzzy Methods
+   * @param value - target
+   * @param min - starting point
+   * @param vertex - vertex of a triangle
+   * @param max - last stop
+   */
+  static triangle(
+    value: number,
+    min: number,
+    vertex: number,
+    max: number
+  ): number {
+    if (value <= min || value >= max) return 0
+    if (value === vertex) return 1
+
+    if (value > min && value < vertex) return Fuzzy.grade(value, min, vertex)
+    return Fuzzy.grade(value, vertex, max, true)
+  }
+
+  /**
+   * Trapezoid of Fuzzy Methods
+   * @param value - target
+   * @param min - starting point
+   * @param vertexStart - near stating point for vertex of trapezoid
+   * @param vertexEnd - near last stop for vertex of trapezoid
+   * @param max - last stop
+   */
+  static trapezoid(
+    value: number,
+    min: number,
+    vertexStart: number,
+    vertexEnd: number,
+    max: number
+  ): number {
+    if (value <= min || value >= max) return 0
+    if (value >= vertexStart && value <= vertexEnd) return 1
+    if (value > min && value < vertexStart)
+      return Fuzzy.grade(value, min, vertexStart)
+    return Fuzzy.grade(value, vertexEnd, max, true)
+  }
+}
